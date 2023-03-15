413d17c2c226438cf1b617c699fc9398178a5762
cliff <cliffowino22@gmail.com>
2023-03-15 21:39:49 +0300

    DNA to amino acid

--- dna_funcs.py
+++ dna_funcs.py
@@ -1,5 +1,5 @@
 from build_blocks import *
-
+from collections import Counter
 
 def validate_seq(seq):
     temp_seq=seq.upper()
@@ -36,3 +36,22 @@ def gc_content_subsection(seq,k=20):
         subseq=seq[i:i+k]
         res.append(gc_count(subseq))
     return res
+
+def translate_seq(seq,init_pos=0):
+    """translate DNA seq to an amino acid seq"""
+    return [DNA_Codons[seq[pos:pos + 3]] for pos in range(init_pos,len(seq) - 2,3)]
+
+def codon_usage(seq,aminoacid):
+    """gives the frequency of each codon coding for a given amino acid"""
+    tempList = []
+    for i in range(0,len(seq) - 2, 3):
+        if DNA_Codons[seq[i:i + 3]] == aminoacid:
+            tempList.append(seq[i:i + 3])
+            print(tempList)
+
+    freqDict = dict(Counter(tempList))
+    print(freqDict)
+    total = sum(freqDict.values())
+    for seq in freqDict:
+        freqDict[seq] = round(freqDict[seq] / total,2)
+    return freqDict
